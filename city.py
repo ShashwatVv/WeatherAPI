f45c718e6ef9558eac134d894f01f2cb19018f78
Shashwat Vaibahav <vaibhavshashwat@gmail.com>
2019-08-29 01:44:08 +0530

    Update city.py

--- city.py
+++ city.py
@@ -1,11 +1,12 @@
 import pyowm
 
 
-owm = pyowm.OWM('a186f048c4365e79c02438c9dffb2295')
+owm = pyowm.OWM('a186f048c4365e79c02438c9dffb2295') #pyowm.OWM('API_KEY')
+#This API_KEY is provided by https://openweathermap.org/
 
 obs = owm.weather_at_place('Delhi')
 weather = obs.get_weather()
 
-temperature = weather.get_temperature(unit= 'celsius')['temp']
+temperature = weather.get_temperature(unit= 'celsius')['temp'] # unit = 'fahrenheit' (alternative option)
 
-print('The Temperature is '+ str(temperature) + 'degrees celsius' )
+print('The Temperature is '+ str(temperature) + 'degrees Celsius' )
